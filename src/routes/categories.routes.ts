0240c90f04ed0254155a14896f9aec0070d8e6b8
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2021-03-16 19:45:53 -0300

    :rocket: Listando as categorias

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -13,4 +13,10 @@ categoriesRoutes.post('/', (request: Request, response: Response) => {
   return response.status(201).send();
 });
 
+categoriesRoutes.get('/', (request: Request, response: Response) => {
+  const all = categoriesRepository.list();
+
+  return response.json(all);
+});
+
 export { categoriesRoutes };
