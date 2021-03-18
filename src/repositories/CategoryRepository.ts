dee8593d0c0bbb569f35e3a87b5d146eeaa7ec5a
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2021-03-18 18:34:35 -0300

    :rocket: Utilizando o Princípio da Substituição de Liskov (LSP)

--- src/repositories/CategoryRepository.ts
+++ src/repositories/CategoryRepository.ts
@@ -1,11 +1,10 @@
 import { Category } from '../model/Category';
+import {
+  ICategoriesRepository,
+  ICreateCategoryDTO,
+} from './ICategoryRepository';
 
-interface ICreateCategoryDTO {
-  name: string;
-  description: string;
-}
-
-class CategoriesRepository {
+class CategoriesRepository implements ICategoriesRepository {
   private categories: Category[];
 
   constructor() {
