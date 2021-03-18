a29cb1657ed84e90e7c42297e3f7b82a6417f8ed
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2021-03-18 18:23:22 -0300

    :rocket: Utilizando o Princípio de Responsabilidade Única (SRP)

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,6 +1,7 @@
 import { Router, Request, Response } from 'express';
 
 import { CategoriesRepository } from '../repositories/CategoryRepository';
+import { CreateCategoryService } from '../services/CreateCategoryService';
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
@@ -8,13 +9,9 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post('/', (request: Request, response: Response) => {
   const { name, description } = request.body;
 
-  const categoryAlreadyExists = categoriesRepository.findByName(name);
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
 
-  if (categoryAlreadyExists) {
-    return response.status(400).json({ error: 'Category already exists!' });
-  }
-
-  categoriesRepository.create({ name, description });
+  createCategoryService.execute({ name, description });
 
   return response.status(201).send();
 });
