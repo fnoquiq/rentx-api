92be2c086b1b2c9dccb21176c5ace50fc4387a8d
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2021-03-19 15:13:13 -0300

    :rocket: Refatorando a listagem de categoria

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,19 +1,16 @@
 import { Router, Request, Response } from 'express';
 
-import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository';
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
+import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post('/', (request: Request, response: Response) => {
   return createCategoryController.handle(request, response);
 });
 
 categoriesRoutes.get('/', (request: Request, response: Response) => {
-  const all = categoriesRepository.list();
-
-  return response.json(all);
+  return listCategoriesController.handle(request, response);
 });
 
 export { categoriesRoutes };
