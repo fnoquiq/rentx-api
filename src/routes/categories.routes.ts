0b90f7e46f413d070bee221f2bdd3969017d2860
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2021-03-19 18:58:31 -0300

    :rocket: Criando upload de arquivos

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,14 @@
 import { Router, Request, Response } from 'express';
+import multer from 'multer';
 
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
 import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 
 const categoriesRoutes = Router();
+
+const upload = multer({
+  dest: './tmp',
+});
 
 categoriesRoutes.post('/', (request: Request, response: Response) => {
   return createCategoryController.handle(request, response);
@@ -12,5 +17,15 @@ categoriesRoutes.post('/', (request: Request, response: Response) => {
 categoriesRoutes.get('/', (request: Request, response: Response) => {
   return listCategoriesController.handle(request, response);
 });
+
+categoriesRoutes.post(
+  '/import',
+  upload.single('file'),
+  (request: Request, response: Response) => {
+    const { file } = request;
+    console.log(file);
+    return response.send();
+  }
+);
 
 export { categoriesRoutes };
