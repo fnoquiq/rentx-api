ee0469590ec2e78d6e777d845a6d3134cbc497fa
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2021-03-25 18:39:18 -0300

    :rocket: Criando migration de especificação

--- src/modules/cars/repositories/ISpecificationsRepository.ts
+++ src/modules/cars/repositories/ISpecificationsRepository.ts
@@ -5,8 +5,8 @@ interface ICreateSpecificationDTO {
   description: string;
 }
 interface ISpecificationsRepository {
-  create({ name, description }: ICreateSpecificationDTO): void;
-  findByName(name: string): Specification;
+  create({ name, description }: ICreateSpecificationDTO): Promise<void>;
+  findByName(name: string): Promise<Specification>;
 }
 
 export { ISpecificationsRepository, ICreateSpecificationDTO };
