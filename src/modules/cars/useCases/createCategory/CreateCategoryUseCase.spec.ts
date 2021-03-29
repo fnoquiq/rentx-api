b8c17d2953bceccedcb8316d007d2c771e8e9ae7
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2021-03-29 12:30:41 -0300

    :rocket: Criando o primeiro teste

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,13 @@
+describe('Criar categoria', () => {
+  it('Espero que 2 + 2 seja 4', () => {
+    const soma = 2 + 2;
+
+    expect(soma).toBe(4);
+  });
+
+  it('Espero que 2 + 2 não seja 5', () => {
+    const soma = 2 + 2;
+
+    expect(soma).not.toBe(5);
+  });
+});
