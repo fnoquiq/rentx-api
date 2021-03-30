45a948a47e691e8f718fd14f9c87f519f57ee9c2
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2021-03-30 12:00:00 -0300

    :rocket: TDD na prática

---
+++ src/modules/cars/useCases/createCar/createCarUseCase.spec.ts
@@ -0,0 +1,25 @@
+import { CarsRepositoryInMemory } from '@modules/cars/repositories/in-memory/CarsRepositoryInMemory';
+
+import { CreateCarUseCase } from './createCarUseCase';
+
+let createCarUseCase: CreateCarUseCase;
+let carsRepository: CarsRepositoryInMemory;
+
+describe('Create Car', () => {
+  beforeEach(() => {
+    carsRepository = new CarsRepositoryInMemory();
+    createCarUseCase = new CreateCarUseCase(carsRepository);
+  });
+
+  it('should be able to create a new car', async () => {
+    await createCarUseCase.execute({
+      name: 'Name Car',
+      description: 'Description Car',
+      daily_rate: 100,
+      license_plate: 'ABC-1234',
+      fine_amount: 60,
+      brand: 'Brand',
+      category_id: 'category',
+    });
+  });
+});
