d943ddb57697db9e7d5107c454b36dad3f4edcaa
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2021-04-11 12:51:28 -0300

    :rocket: Caso de uso de reset de senha

---
+++ src/modules/accounts/useCases/resetPasswordUser/ResetPasswordUserUseCase.ts
@@ -0,0 +1,57 @@
+import { hash } from 'bcryptjs';
+import { inject, injectable } from 'tsyringe';
+
+import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
+import { IUsersTokensRepository } from '@modules/accounts/repositories/IUsersTokenRepository';
+import { IDateProvider } from '@shared/container/providers/DateProvider/IDateProvider';
+import { AppError } from '@shared/errors/AppError';
+
+interface IRequest {
+  token: string;
+  password: string;
+}
+
+@injectable()
+class ResetPasswordUserUseCase {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('UsersTokensRepository')
+    private usersTokensRepository: IUsersTokensRepository,
+
+    @inject('DateProvider')
+    private dateProvider: IDateProvider
+  ) {}
+
+  async execute({ token, password }: IRequest): Promise<void> {
+    const userToken = await this.usersTokensRepository.findByRefreshToken(
+      token
+    );
+
+    if (!userToken) {
+      throw new AppError('Token invalid!');
+    }
+
+    console.log(userToken.expires_date, this.dateProvider.dateNow());
+
+    if (
+      this.dateProvider.compareIfBefore(
+        userToken.expires_date,
+        this.dateProvider.dateNow()
+      )
+    ) {
+      throw new AppError('Token expired!');
+    }
+
+    const user = await this.usersRepository.findById(userToken.user_id);
+
+    user.password = await hash(password, 8);
+
+    await this.usersRepository.create(user);
+
+    await this.usersTokensRepository.deleteById(userToken.id);
+  }
+}
+
+export { ResetPasswordUserUseCase };
