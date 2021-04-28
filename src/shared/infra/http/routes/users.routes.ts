09be4cf77f5a681ddc2032e0e4f18d8cd2fdf20b
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2021-04-28 18:48:50 -0300

    :rocket: Criando URL de acesso do avatar

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -3,6 +3,7 @@ import multer from 'multer';
 
 import uploadConfig from '@config/upload';
 import { CreateUserController } from '@modules/accounts/useCases/createUser/CreateUserController';
+import { ProfileUserController } from '@modules/accounts/useCases/profileUserUseCase/ProfileUserController';
 import { UpdateUserAvatarController } from '@modules/accounts/useCases/updateUserAvatar/updateUserAvatarController';
 
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
@@ -13,6 +14,7 @@ const uploadAvatar = multer(uploadConfig);
 
 const createUserController = new CreateUserController();
 const updateUserAvatarController = new UpdateUserAvatarController();
+const profileUserController = new ProfileUserController();
 
 usersRouters.post('/', createUserController.handle);
 
@@ -22,5 +24,7 @@ usersRouters.patch(
   uploadAvatar.single('avatar'),
   updateUserAvatarController.handle
 );
+
+usersRouters.get('/profile', ensureAuthenticated, profileUserController.handle);
 
 export { usersRouters };
